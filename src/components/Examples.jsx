dae718d07c7b5906aa414aba19a2e8bbad2ae001
jeet <jeetbanik332004@gmail.com>
2024-11-15 23:50:19 +0530

    working with multiple jsx

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./tabButton";
 import Section from "./section";
-
+import Tabs from "./Tabs";
 export default function Examples () {
 const [ selectedTopic, setSelectedTopic] = useState();
   function handelSelect(selectedbutton){
@@ -12,11 +12,15 @@ const [ selectedTopic, setSelectedTopic] = useState();
 
     <Section title= "Examples" id="examples">
           {/* <h2>Examples</h2> */}
-         <menu>
-         <TabButton isSelected={selectedTopic == 'components'} onClick={ () => handelSelect('components')}>Component</TabButton>
+          <Tabs buttons={<><TabButton isSelected={selectedTopic == 'components'} onClick={ () => handelSelect('components')}>Component</TabButton>
          <TabButton isSelected={selectedTopic == 'jsx'} onClick={ () => handelSelect('jsx')}>JSX</TabButton>
          <TabButton isSelected={selectedTopic == 'props'} onClick={ () => handelSelect('props')}>Props</TabButton>
-         <TabButton isSelected={selectedTopic == 'state'} onClick={ () => handelSelect('state')}>State</TabButton>
+         <TabButton isSelected={selectedTopic == 'state'} onClick={ () => handelSelect('state')}>State</TabButton></>}></Tabs>
+         <menu>
+         {/* <TabButton isSelected={selectedTopic == 'components'} onClick={ () => handelSelect('components')}>Component</TabButton>
+         <TabButton isSelected={selectedTopic == 'jsx'} onClick={ () => handelSelect('jsx')}>JSX</TabButton>
+         <TabButton isSelected={selectedTopic == 'props'} onClick={ () => handelSelect('props')}>Props</TabButton>
+         <TabButton isSelected={selectedTopic == 'state'} onClick={ () => handelSelect('state')}>State</TabButton> */}
          </menu>
          {!selectedTopic ? <p>Please select a topic </p> : <div id="tab-content">
           <h3>{EXAMPLES[selectedTopic].title}</h3>
